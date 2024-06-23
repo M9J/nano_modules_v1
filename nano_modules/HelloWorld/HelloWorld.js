71e98e8cbde2baadf44f149f61b3230d18dadc9b
Martian Jeirgif <mgj.exe@gmail.com>
2024-06-23 21:51:23 +0530

    gitignore cleanup

--- nano_modules/HelloWorld/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-export default class HelloWorld {
-  MODULE_NAME = "Demo First Program";
-  MODULE_DESCRIPTION = "First Custom Nano Module";
-  MODULE_VERSION = "1.0";
-  MODULE_MAIN = () => "Hello world";
-}
--- nano_modules/index.js
+++
@@ -1,5 +0,0 @@
-import HelloWorld from "./HelloWorld/HelloWorld.js";
-import TodayDate from "./TodayDate/TodayDate.js";
-
-export default [TodayDate, HelloWorld];
-// export default [];
